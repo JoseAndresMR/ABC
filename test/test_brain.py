b16a23553d452c9776a272e4ba350a6b52b12c9c
Carlos Perales <sir.perales@gmail.com>
2021-12-15 11:22:21 +0100

    Config dict for all methods in TestNeuron

--- test/test_brain.py
+++ test/test_brain.py
@@ -2,12 +2,20 @@ from brainrl.brain import Neuron
 import unittest
 
 class TestNeuron(unittest.TestCase):
-    def test_instance(self):
-        neuron = Neuron(neuron_type='intern',
-                 config={'ID': '1',
+    @staticmethod
+    def get_config():
+        """
+        Same config for all the tests.
+        """
+        return {'ID': '1',
                         'agent': {'ID': 1,
                                   'type': 'Other'}
-                        },
+                        }
+
+
+    def test_instance(self):
+        neuron = Neuron(neuron_type='intern',
+                 config=self.get_config(),
                  log_path='',
                  k_dim=2,
                  v_dim=2,
@@ -17,10 +25,7 @@ class TestNeuron(unittest.TestCase):
     def test_error_type(self):
         with self.assertRaises(ValueError):
             neuron = Neuron(neuron_type='non_valid_type',
-                 config={'ID': '1',
-                        'agent': {'ID': 1,
-                                  'type': 'Other'}
-                        },
+                 config=self.get_config(),
                  log_path='',
                  k_dim=2,
                  v_dim=2,
