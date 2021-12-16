122746fb738d6dd973fd15d0f7d53943b39922d5
Carlos Perales <sir.perales@gmail.com>
2021-12-16 16:07:01 +0100

    Test for neuron finished

--- test/test_brain.py
+++ test/test_brain.py
@@ -69,38 +69,14 @@ class TestNeuron(unittest.TestCase):
         neuron.set_next_input_value(np.random.random((1, 2)))
         action = neuron.forward()
 
-    # def test_decompose(self):
-    #     neuron_sensory_motor = Neuron(neuron_type='sensory-motor',
-    #                                   config=self.get_config(),
-    #                                   log_path='',
-    #                                   k_dim=2,
-    #                                   v_dim=2,
-    #                                   environment_signal_size=[2, 2])
-    #     neuron_sensory_motor.decompose_action()
-
-    #     neuron_sensory = Neuron(neuron_type='sensory',
-    #                                   config=self.get_config(),
-    #                                   log_path='',
-    #                                   k_dim=2,
-    #                                   v_dim=2,
-    #                                   environment_signal_size=[2, 2])
-    #     neuron_sensory.decompose_action()
-        
-    #     neuron_intern = Neuron(neuron_type='intern',
-    #                                   config=self.get_config(),
-    #                                   log_path='',
-    #                                   k_dim=2,
-    #                                   v_dim=2,
-    #                                   environment_signal_size=[2, 2])
-    #     neuron_intern.decompose_action()
-
-    #     neuron_motor = Neuron(neuron_type='motor',
-    #                                   config=self.get_config(),
-    #                                   log_path='',
-    #                                   k_dim=2,
-    #                                   v_dim=2,
-    #                                   environment_signal_size=[2, 2])
-    #     neuron_motor.decompose_action()
+    def test_decompose(self):
+        neuron_sensory_motor = Neuron(neuron_type='sensory-motor',
+                                      config=self.get_config(),
+                                      log_path='',
+                                      k_dim=2,
+                                      v_dim=2,
+                                      environment_signal_size=[2, 2])
+        neuron_sensory_motor.decompose_action()
     
 
 if __name__ == '__main__':
