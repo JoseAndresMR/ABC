2f0e8fe33d614a19255cee26e60a4a0e219c6516
Carlos Perales <sir.perales@gmail.com>
2022-01-04 17:03:09 +0100

    Tests for management finished

--- brainrl/management/test_bench.py
+++ brainrl/management/test_bench.py
@@ -5,11 +5,10 @@ import shutil
 from typing import NewType
 import optuna
 
-from .Experience import Experience
 from brainrl.management.experience import Experience
 
 
-def setValueInDictWithPath(dic, keys, value):
+def set_value_in_dict_with_path(dic, keys, value):
     for key in keys[:-1]:
         if type(dic) == dict:
             dic = dic.setdefault(key, {})
@@ -58,12 +57,12 @@ class TestBench(object):
                 path = prepath + (k,)
                 if type(v) == str and len(v) >= 5 and v[-5:] == ".json": # found json
                     with open(os.path.join(self.config_path,v), 'r') as j:
-                        setValueInDictWithPath(self.base_config, path, json.load(j))
+                        set_value_in_dict_with_path(self.base_config, path, json.load(j))
                     v = nested_dict[k]
                 if hasattr(v, 'items') or type(v) == list: # v is a dict or list
                     self.expandJsons(v, path) # recursive call
 
-    def experiences(self):
+    def experiences(self, max_iterations=999999999999, n_trials=15):
         """ Run all experiments in sequence, changing the alterations from the prior base configuration. """
         i = 0
         for experiment_config in self.base_config["experiments"]:
@@ -71,10 +70,10 @@ class TestBench(object):
                 for variable_name, variable_params in self.test_variables.items():
                     suggested_values = variable_params["suggested_values"]
                     x = trial.suggest_float(variable_name, suggested_values[0], suggested_values[1])
-                    setValueInDictWithPath(current_config, variable_params["path"], x)
+                    set_value_in_dict_with_path(current_config, variable_params["path"], x)
 
                 exp = Experience(current_config, current_log_path)
-                performance = exp.loop()
+                performance = exp.loop(max_iterations=max_iterations)
                 exp.finish()
                 return performance
 
---
+++ test/management/test_bench.py
@@ -0,0 +1,20 @@
+from genericpath import exists
+import unittest
+import os
+import numpy as np
+
+from brainrl.management.test_bench import TestBench
+
+
+class TestTestBench(unittest.TestCase):
+    def test_instance(self):
+        tb = TestBench(config_folder_path='config', data_folder_path='data')    
+        self.assertIsInstance(tb, TestBench)
+
+    def test_experiences(self):
+        tb = TestBench(config_folder_path='config', data_folder_path='data')  
+        tb.experiences(max_iterations=3, n_trials=5)
+
+
+if __name__ == '__main__':
+    unittest.main()
